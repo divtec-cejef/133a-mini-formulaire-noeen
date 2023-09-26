3bc95ceb6028bd22df6507f97ee948faa726e8a6
Paula <paula@pena-parra.ch>
2023-09-26 15:30:22 +0200

    fin code js

--- js/main.js
+++ js/main.js
@@ -39,10 +39,28 @@ const txtPasswordConfirm = formulaire.querySelector("#confirm");
 console.log(formulaire, txtPassword, txtPasswordConfirm);
 
 // Ecoute l'envoi du formulaire
-formulaire.addEventListener("submit", () => {
+formulaire.addEventListener("submit", (event) => {
+    // Stopper l'envoi ou annuler événement
+    event.preventDefault();
+    // Si mot de passe < 3
+    if (txtPassword.value.length < 3) {
+        alert("Le mot de passe doit avoir au minimum 3 caractères !");
+        return;
+    }
+    // Si mot de passe et confirmation ne correspondent pas
     if (txtPassword.value !== txtPasswordConfirm.value) {
         alert("mots de passe différents !");
         return; //Termine la fonction
     }
+    // Informe l'utilisateur et envoie le formulaire
     alert("compte créé avec succès");
-})
+    formulaire.submit();
+});
+
+/*****
+ Mécanique d'un formulaire:
+    - écoute l'envoi du formulaire
+    - stoppe l'envoi du formulaire
+    - tests
+    - envoi du formulaire
+ *****/
